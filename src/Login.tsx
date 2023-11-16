f5b9668871e1b91e4f63063537b4e6f2a7d40720
Oscar Ore <oscar.ore2011@gmail.com>
2023-11-16 17:29:50 -0500

    added text fields to login page

--- src/Login.tsx
+++ src/Login.tsx
@@ -4,18 +4,50 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export const Login: React.FC = () => {
 	return (
-		<Box>
+		<Box
+			sx={{
+				backgroundColor: "skyblue",
+				margin: "1rem 1rem 1rem 1rem",
+				padding: "5rem 1rem 25rem 1rem",
+			}}
+		>
 			<AccountCircleIcon />
-            <Typography sx={{
-                margin: '1rem 1rem 1rem 1rem'
-            }}
-            >Login Page
-            </Typography>
+			<Typography
+				sx={{
+					margin: "1rem 1rem 1rem 1rem",
+				}}
+			>
+				Login Page
+			</Typography>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center", // Center items vertically
+				}}
+			>
+				<TextField
+					label='Username'
+					required
+					sx={{
+						width: "50rem",
+						mb: 2,
+					}}
+				>
+					UserName
+				</TextField>
 
-            <TextField
-            >
-                UserName
-            </TextField>
+				<TextField
+					label='Password'
+					required
+					sx={{
+						width: "50rem",
+						mb: 2,
+					}}
+				>
+					Password
+				</TextField>
+			</Box>
 		</Box>
 	);
 };
