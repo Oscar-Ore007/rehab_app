c15d306e1b7bb559da406b74cec48f7f16719e98
Oscar Ore <oscar.ore2011@gmail.com>
2023-11-23 12:01:45 -0500

    added imports to App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Login } from "./Login";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import { Profile } from "./Profile";
 
 export const App: React.FC = () => {
 	return (
 		<>
-      <Login />
-      {/* <Profile />  */}
+			<Login />
+			{/* <Profile />  */}
 		</>
 	);
 };
