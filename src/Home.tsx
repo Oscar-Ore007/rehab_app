298c68ece038ae2ab97342fe4d1d5f9dc09e42f3
Oscar Ore <oscar.ore2011@gmail.com>
2023-12-25 17:47:02 -0500

    added nav bar component to home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,11 @@
 import { Box, Paper } from "@mui/material";
 import React from "react";
+import { Navbar } from "./Navbar";
 
-export const Profile: React.FC = () => {
+export const Home: React.FC = () => {
 	return (
 		<Paper>
-			<div>
-				Hello World 
-			</div>
+		<Navbar />
 		</Paper>
-	) 
+	);
 };
