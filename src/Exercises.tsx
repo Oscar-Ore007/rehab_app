ad7e0984bc3089751c6cc2e0893960f5f6f94744
Oscar Ore <oscar.ore2011@gmail.com>
2024-02-28 22:13:30 -0500

    adjusting exercise page

--- src/Exercises.tsx
+++ src/Exercises.tsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Card, Grid, Paper } from "@mui/material";
+import { Card, Grid, Paper, Typography } from "@mui/material";
 
 export const Exercises: React.FC = () => {
 	return (
 		<Grid>
-			<Paper>
+			<Paper
+				sx={{
+					borderRadius: "50px",
+				}}
+			>
 				<Card
 					sx={{
 						width: "100%",
-						height: "100%",
+						height: "100rem",
+						borderRadius: "50px",
 					}}
-				>k
-					<div>Hello World</div>
+				>
+					<Typography
+						sx={{
+							margin: "4rem",
+						}}
+					>
+						Exercises
+					</Typography>
+					{/* //You could also probably make a grid  */}
 				</Card>
 			</Paper>
 		</Grid>
